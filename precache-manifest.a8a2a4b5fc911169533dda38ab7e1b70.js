4fa5654dfe5862a7c6101b9946d7e2879eff62e0
Min Khant Kyaw <minkhantkyaw.na@gmail.com>
2019-09-29 20:22:53 -0400

    Updates

---
+++ precache-manifest.a8a2a4b5fc911169533dda38ab7e1b70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7656f12e806b1a068ea5e3d857bac3e9",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "d1e001d4759b19a34510",
+    "url": "/clicky-game/static/css/main.708c7a18.chunk.css"
+  },
+  {
+    "revision": "569a54ee2a9d4cd864ca",
+    "url": "/clicky-game/static/js/2.ffd3a78d.chunk.js"
+  },
+  {
+    "revision": "d1e001d4759b19a34510",
+    "url": "/clicky-game/static/js/main.725b692b.chunk.js"
+  },
+  {
+    "revision": "0826f9c9377742a6e5c7",
+    "url": "/clicky-game/static/js/runtime-main.2c299f57.js"
+  }
+]);
